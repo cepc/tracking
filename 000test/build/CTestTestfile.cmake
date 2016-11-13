f5b4bea06301cf23dc785f0961d3f342011d7ad2
mzhao <zhaomr13@mails.tsinghua.edu.cn>
2016-11-13 10:27:55 +0800

    delete build

--- 000test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhaomr/workdir/cepc/Tracking/000test
-# Build directory: /home/zhaomr/workdir/cepc/Tracking/000test/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
